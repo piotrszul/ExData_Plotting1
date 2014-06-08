65af3da6147dcd010c088143892764b8c94dc8b6
Piotr Szul <piotr.szul@csiro.au>
2014-06-09 09:24:58 +1000

    Added partial plots 2 and 3

---
+++ plot2.R
@@ -0,0 +1,8 @@
+data <- read.table(unz("household_power_consuption.zip", "household_power_consumption.txt"), sep=";", header=TRUE, na.strings= "?")
+focus_data <-data[data$Date %in% c("1/2/2007", "2/2/2007"), ]
+focus_data$RDate = as.Date(focus_data$Date,"%d/%m/%Y")
+png("plot2.png",504,504)
+plot(focus_data$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.off()
+
+
