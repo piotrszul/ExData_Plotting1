67b1ca701abac4946a0dca8e9ef6db94781069e2
Piotr Szul <piotr.szul@csiro.au>
2014-08-05 20:32:45 +1000

    Added correct axis to 2 and 3

--- plot2.R
+++ plot2.R
@@ -2,7 +2,7 @@ data <- read.table(unz("household_power_consuption.zip", "household_power_consum
 focus_data <-data[data$Date %in% c("1/2/2007", "2/2/2007"), ]
 focus_data$RDate = as.Date(focus_data$Date,"%d/%m/%Y")
 png("plot2.png",504,504)
-plot(focus_data$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+plot(focus_data$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="", xaxt="n")
+axis(1, c(0,length(focus_data$Global_active_power)/2, length(focus_data$Global_active_power)), c("Tue", "Fri", "Sat"))
 dev.off()
-
 
--- plot3.R
+++ plot3.R
@@ -1,10 +1,10 @@
 data <- read.table(unz("household_power_consuption.zip", "household_power_consumption.txt"), sep=";", header=TRUE, na.strings= "?")
 focus_data <-data[data$Date %in% c("1/2/2007", "2/2/2007"), ]
 focus_data$RDate = as.Date(focus_data$Date,"%d/%m/%Y")
-png("plot3.png",504,504)
-plot(focus_data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+#png("plot3.png",504,504)
+plot(focus_data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="", xaxt="n")
+axis(1, c(0,length(focus_data$Global_active_power)/2, length(focus_data$Global_active_power)), c("Tue", "Fri", "Sat"))
 lines(focus_data$Sub_metering_2, col="red")
 lines(focus_data$Sub_metering_3, col="blue")
-dev.off()
-
-
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red","blue"))
+#dev.off()
